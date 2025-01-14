9522ba173e160f6249dc2e98ca99361fec0952e8
dna020 <dna020@naver.com>
2025-01-14 21:19:53 +0900

    1.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <main className="main-container">
-        <h1>예산 계산기</h1>
+        <h1>예산 계산기11</h1>
 
         <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
           {/* Expense Form */}
