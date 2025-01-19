756f5d83336a26e7880e660b9d014d33d9e0ffa0
dna020 <dna020@naver.com>
2025-01-19 23:05:06 +0900

    '1.4'

--- src/App.js
+++ src/App.js
@@ -1,35 +1,69 @@
-import { Component, useState } from "react";
+import { useState } from "react";
 import './App.css';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from "./components/ExpenseList";
-import { GiConsoleController } from "react-icons/gi";
 
 const App = () => {
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState(0);
+
   const [expenses, setExpenses] = useState([
     { id: 1, charge: "렌트비", amount: 1600},
     { id: 2, charge: "교통비", amount: 400},
     { id: 3, charge: "식비", amount: 1200}
   ])
 
-  handleDelete = (id) => {
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  }
+
+  const handleAmount = (e) => {
+    // console.log(typeof e.target.valueAsNumber)
+    setAmount(e.target.valueAsNumber);
+  }
+
+  const handleDelete = (id) => {
     const newExpenses = expenses.filter(expense => expense.id !== id);
     
     setExpenses(newExpenses);
   }
   
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(charge !== "" && amount > 0) {
+      // const newExpense = {id: crypto.randomUUID(), charge: charge, amount: amount};
+      const newExpense = {id: crypto.randomUUID(), charge, amount};
+
+      //불변성을 지켜주기 위해서 새로운 expenses를 생성
+      const newExpenses = [...expenses, newExpense];
+      setExpenses(newExpenses);
+      setCharge("");
+      setAmount(0);
+
+    } else {
+      console.log('error');
+    }
+  }
+
   return (
     <main className="main-container">
       <h1>예산 계산기</h1>
 
       <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
         {/* Expense Form */}
-        <ExpenseForm />
+        <ExpenseForm
+        handleCharge = {handleCharge}
+        charge = {charge}
+        handleAmount = {handleAmount}
+        amount = {amount}
+        handleSubmit = {handleSubmit}
+        />
       </div>
 
       <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
         {/* Expense List */}
         <ExpenseList
-        initalExpenses={expenses}
+        initialExpenses={expenses}
         handleDelete={handleDelete}
         />
       </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,28 +2,22 @@ import React, { Component } from 'react'
 import './ExpenseItem.css';
 import { MdDelete, MdEdit } from 'react-icons/md';
 
-const ExpenseItem = () => {
-  
-}
-
-export class ExpenseItem extends Component {
-  render() {
-    return (
-      <li className='item'>
-        <div className='info'>
-          <span className='expense'>{this.props.expense.charge}</span>
-          <span className='abount'>{this.props.expense.amount}</span>
-        </div>
-        <div>
-          <button className='edit-btn'><MdEdit /></button>
-          <button className='clear-btn' onClick={() => 
-            this.props.handleDelete(this.props.expense.id)
-          }>
-            <MdDelete /></button>
-        </div>
-      </li>
-    )
-  }
+const ExpenseItem = ({expense, handleDelete}) => {
+  return (
+    <li className='item'>
+      <div className='info'>
+        <span className='expense'>{expense.charge}</span>
+        <span className='abount'>{expense.amount}</span>
+      </div>
+      <div>
+        <button className='edit-btn'><MdEdit /></button>
+        <button className='clear-btn' onClick={() => 
+          handleDelete(expense.id)
+        }>
+          <MdDelete /></button>
+      </div>
+    </li>
+  )
 }
 
 export default ExpenseItem
